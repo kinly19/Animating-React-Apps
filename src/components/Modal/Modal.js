6613687f8dc3a68e3583d1383f8d395c1e083818
unknown <kinchoily@hotmail.com>
2021-11-29 18:43:56 +0000

    Modal.js - Add comments/notes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 import './Modal.css';
+// ============================== Notes ==============================
+// react-transition-group - Exposes simple components useful for defining 
+//  entering and exiting transitions. React Transition Group is not an animation library like React-Motion, 
+//  it does not animate styles by itself. Instead it exposes transition stages, manages classes and group elements 
+//  and manipulates the DOM in useful ways, making the implementation of actual visual transitions much easier.
+//  https://reactcommunity.org/react-transition-group/ 
+
+// Transition - component lets you describe a transition from one component state to another over time with a simple declarative API
+//  Most commonly it's used to animate the mounting and unmounting of a component
+//  https://reactcommunity.org/react-transition-group/transition
+
+
+// join() - method creates and returns a new string by concatenating... 
+//  all of the elements in an array 
+// ===================================================================
 
 const modal = (props) => {
 
